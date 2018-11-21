915c8eb8d404b2693369446a4b6519fb79d0b433
juliencegarra <juliencegarra@gmail.com>
2018-11-21 15:47:59 +0100

    Fix translation

--- Helpers/Translator.py
+++ Helpers/Translator.py
@@ -12,23 +12,28 @@ _translations = {}
 _lang = ""
 
 def translate(s):
+    global _translations
+    global _lang
+    _lang = _lang.strip()
     if len(_translations)==0 and _lang!='':
+
         translation_filename = os.path.join('Translations', _lang + ".txt")
 
         if not os.path.exists(translation_filename):
-            print "There is no translation in the "+lang+" language!"
-            return
-        try:
-            with open(translation_filename, 'r') as lines:
-                for line in lines:
-                    split = line.decode('utf-8').split('=')
-                    if len(split)==2:
-                        _translations[split[0]]=''.join(split[1].splitlines()) #remove carriage return
-        except:
-            OSCriticalErrorMessage("Error",
-            "Unable to open the translation. Please ensure it is saved as an UTF-8 file!")
+            print "There is no translation in the '"+_lang+"' language!"
+            _lang = "" # reset the translation to resume
+        else:
+            try:
+                with open(translation_filename, 'r') as lines:
+                    for line in lines:
+                        split = line.decode('utf-8').split('=')
+                        if len(split)==2:
+                            _translations[split[0]]=''.join(split[1].splitlines()) #remove carriage return
+            except:
+                OSCriticalErrorMessage("Error",
+                "Unable to open the translation. Please ensure it is saved as an UTF-8 file!")
 
-    if _translations.has_key(unicode(s)):
+    if len(_translations)!=0 and translations.has_key(unicode(s)):
         return unicode(_translations[unicode(s)])
 
     return unicode(s)
