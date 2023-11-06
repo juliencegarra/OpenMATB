1a0fee4362eb515d3a0641a401f6fcfa8cadf221
Psy-lzh <134902447+Psy-lzh@users.noreply.github.com>
2023-11-06 11:50:04 +0800

    Update utils.py
    Bug fixed: when setting your own font, there was an error "font is not defined"

--- core/utils.py
+++ core/utils.py
@@ -1,7 +1,7 @@
 # Copyright 2023, by Julien Cegarra & Benoît Valéry. All rights reserved.
 # Institut National Universitaire Champollion (Albi, France).
 # License : CeCILL, version 2.1 (see the LICENSE file)
-
+from pyglet import font
 from core.constants import PATHS as P
 import configparser
 
