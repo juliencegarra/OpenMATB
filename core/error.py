584b1090d643d5235a71874f7e329dba3f40e8a0
juliencegarra <juliencegarra@gmail.com>
2024-03-09 12:51:11 +0100

    Start uncoupling window

--- core/error.py
+++ core/error.py
@@ -1,10 +1,11 @@
 from core.modaldialog import ModalDialog
+import core.window
 
 class Errors:
 	def __init__(self):
 		self.errors_list = list()
 		self.some_fatals = False
-		self.win = None
+
 
 	def add_error(self, error_msg, fatal=False):
 		self.some_fatals = max(self.some_fatals, fatal)
@@ -16,13 +17,13 @@ class Errors:
 
 
 	def show_errors(self):
-		if self.win is not None:
+		if MainWindow is not None:
 			if not self.is_empty():
 				pass_list = list(self.errors_list)
 				self.errors_list = list()
 				title = _('Warning') if not self.some_fatals else _('Error(s)')
 				continue_key = None if self.some_fatals else 'SPACE'
-				self.win.modal_dialog = ModalDialog(self.win, pass_list, title=title,
+				MainWindow.modal_dialog = ModalDialog(MainWindow, pass_list, title=title,
 					  								continue_key=continue_key, exit_key='Q')
 
 errors = Errors()
