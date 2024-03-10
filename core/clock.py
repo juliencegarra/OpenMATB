59a78f768cdaeee26f9976eb2b620c25f4ff2800
juliencegarra <juliencegarra@gmail.com>
2024-03-10 19:20:25 +0100

    Fix clock target time

--- core/clock.py
+++ core/clock.py
@@ -1,4 +1,4 @@
-# Copyright 2023, by Julien Cegarra & Benoît Valéry. All rights reserved.
+# Copyright 2023-2024, by Julien Cegarra & Benoît Valéry. All rights reserved.
 # Institut National Universitaire Champollion (Albi, France).
 # License : CeCILL, version 2.1 (see the LICENSE file)
 
@@ -21,11 +21,9 @@ class Clock(pyglet.clock.Clock):
     def advance(self, time: float):
         self.set_time(self._time + time)
         self.tick()
-        if self.target_time is not None:
+        if self.is_target_time_defined():
             if self.target_time >= self.get_time():
                 self.advance(0.1)   # Recursion
-            else:
-                self.target_time = None
 
 
     def get_time(self) -> float:
@@ -36,25 +34,29 @@ class Clock(pyglet.clock.Clock):
     def set_time(self, time: float):
         self._time = time
 
+    def remove_target_time(self):
+        self.target_time = None
 
     def set_target_time(self, target_time):
+        target_time += self.get_time()
+
         if target_time < self.get_time():
             print('Warning. The clock was sent a bad target time (back in time)')
+
         self.target_time = target_time
-        return self.target_time
 
 
-    def is_target_defined(self):
+    def is_target_time_defined(self) -> bool:
         return self.target_time is not None
 
 
     def get_target_time(self):
-        if self.is_target_defined():
+        if self.is_target_time_defined():
             return self.target_time
         return
 
 
-    def is_target_reached(self):
+    def is_target_time_reached(self):
         if self.target_time is not None:
             if self.target_time <= self.get_time():
                 return True
