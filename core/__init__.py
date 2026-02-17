ae29e600f15d074d441d5cbb3023e710ea6ad62f
Julien Cegarra <juliencegarra@users.noreply.github.com>
2026-02-17 13:39:34 +0100

    Fix import

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,8 @@
 # Copyright 2023, by Julien Cegarra & Benoît Valéry. All rights reserved.
 # Institut National Universitaire Champollion (Albi, France).
-# License : CeCILL, version 2.1 (see the LICENSE file)from .constants import FONT_SIZES, COLORS, Group
+# License : CeCILL, version 2.1 (see the LICENSE file)
+
+from .constants import FONT_SIZES, COLORS, Group
 
 from .logreader import LogReader
 from .logger import logger
