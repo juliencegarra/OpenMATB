aed8c6c4f823d357cb18bed60ac58061f7c33630
Julien Cegarra <juliencegarra@users.noreply.github.com>
2026-02-22 21:52:08 +0100

    Update window.py

--- core/window.py
+++ core/window.py
@@ -67,6 +67,7 @@ class Window(Window):
         self.alive: bool = True
         self.modal_dialog: ModalDialog | None = None
         self.slider_visible: bool = False
+        self.mouse_control_active: bool = False
 
         self.on_key_press_replay: Any | None = None  # used by the replay
 
@@ -143,7 +144,7 @@ class Window(Window):
         self.batch.draw()
 
     def is_mouse_necessary(self) -> bool:
-        return self.slider_visible or REPLAY_MODE
+        return self.slider_visible or REPLAY_MODE or self.mouse_control_active
 
     # Log any keyboard input, either plugins accept it or not
     # is subclassed in replay mode
